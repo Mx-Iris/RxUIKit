c5c2959ce5eeb0d67a584775dbf40412a9669bcf
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-06-27 01:05:25 +0800

    Updates

--- Sources/RxUIKit/UICollectionView+Rx.swift
+++ Sources/RxUIKit/UICollectionView+Rx.swift
@@ -22,29 +22,74 @@ enum OutlineSection {
 
 @available(iOS 14.0, *)
 class OutlineCollectionViewDiffableDataSource<Node: OutlineNodeType & Hashable>: UICollectionViewDiffableDataSource<OutlineSection, Node>, RxCollectionViewDataSourceType, SectionedViewDataSourceType where Node.NodeType == Node {
-    
-    typealias Element = [Node]
-    
-    func collectionView(_ collectionView: UICollectionView, observedEvent: RxSwift.Event<Element>) {
-        Binder(self) { target, nodes in
-            var snapshot = NSDiffableDataSourceSectionSnapshot<Node>()
-            func addNodes(_ children: [Node], to parent: Node?) {
-                snapshot.append(children, to: parent)
-                for child in children where child.isExpandable {
-                    addNodes(child.children, to: child)
-                }
-            }
+    typealias ConfigureCell = (UICollectionView, IndexPath, Node, UICollectionViewListCell) -> Void
 
-            addNodes(nodes, to: nil)
-            target.apply(snapshot, to: .main, animatingDifferences: false)
+    typealias Snapshot = NSDiffableDataSourceSectionSnapshot<Node>
+
+    typealias Element = [Node]
+
+    private var currentSnapshot: Snapshot?
+
+    private var filteredSnapshot: Snapshot?
+
+    init(collectionView: UICollectionView, configureCell: @escaping ConfigureCell) {
+        let headerCellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Node> { cell, indexPath, node in
+            configureCell(collectionView, indexPath, node, cell)
+            cell.accessories = [.outlineDisclosure(options: .init(style: .header))]
         }
-        .on(observedEvent)
+
+        let listCellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Node> { cell, indexPath, node in
+            configureCell(collectionView, indexPath, node, cell)
+        }
+
+        super.init(collectionView: collectionView) { collectionView, indexPath, node -> UICollectionViewCell? in
+            if node.isExpandable {
+                return collectionView.dequeueConfiguredReusableCell(using: headerCellRegistration, for: indexPath, item: node)
+            } else {
+                return collectionView.dequeueConfiguredReusableCell(using: listCellRegistration, for: indexPath, item: node)
+            }
+        }
     }
 
-    
+    func collectionView(_ collectionView: UICollectionView, observedEvent event: RxSwift.Event<Element>) {
+        Binder(self) { target, nodes in
+            let snapshot = target.snapshot(for: nodes)
+            target.apply(snapshot, to: .main, animatingDifferences: false)
+            target.currentSnapshot = snapshot
+        }
+        .on(event)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, observedFilteredEvent event: RxSwift.Event<Element?>) {
+        Binder(self) { target, nodes in
+            if let nodes {
+                let snapshot = target.snapshot(for: nodes)
+                target.apply(snapshot, to: .main, animatingDifferences: false)
+                target.filteredSnapshot = snapshot
+            } else if let currentSnapshot = target.currentSnapshot {
+                target.apply(currentSnapshot, to: .main, animatingDifferences: false)
+                target.filteredSnapshot = nil
+            }
+        }
+        .on(event)
+    }
+
+    func snapshot(for nodes: [Node]) -> Snapshot {
+        var snapshot = Snapshot()
+        func addNodes(_ children: [Node], to parent: Node?) {
+            snapshot.append(children, to: parent)
+            for child in children where child.isExpandable {
+                addNodes(child.children, to: child)
+            }
+        }
+
+        addNodes(nodes, to: nil)
+        return snapshot
+    }
+
     func model(at indexPath: IndexPath) throws -> Any {
         precondition(indexPath.section == 0)
-        
+
         guard let item = itemIdentifier(for: indexPath) else {
             throw RxCocoaError.itemsNotYetBound(object: self)
         }
@@ -52,40 +97,52 @@ class OutlineCollectionViewDiffableDataSource<Node: OutlineNodeType & Hashable>:
     }
 }
 
+import OSLog
+
+@available(iOS 14.0, *)
+private let logger = Logger(subsystem: "com.JH.RxUIKit", category: "UICollectionView")
+
+@available(iOS 14.0, *)
 extension Reactive where Base: UICollectionView {
-    
-    @available(iOS 14.0, *)
     public func rootNode<OutlineNode: OutlineNodeType & Hashable, Source: ObservableType>(source: Source)
-        -> (@escaping (UICollectionView, IndexPath, OutlineNode) -> UICollectionViewCell?)
+        -> (@escaping (UICollectionView, IndexPath, OutlineNode, UICollectionViewListCell) -> Void)
         -> Disposable
         where OutlineNode.NodeType == OutlineNode, Source.Element == OutlineNode {
-        return { cellProvider in
-            _ = self.dataSource
-            let dataSource = OutlineCollectionViewDiffableDataSource(collectionView: base, cellProvider: cellProvider)
-            return self.nodes(dataSource: dataSource)(source.map { [$0] })
-        }
+            nodes(source: source.map { [$0] })
     }
-    
-    @available(iOS 14.0, *)
+
     public func nodes<OutlineNode: OutlineNodeType & Hashable, Source: ObservableType>(source: Source)
-        -> (@escaping (UICollectionView, IndexPath, OutlineNode) -> UICollectionViewCell?)
+        -> (@escaping (UICollectionView, IndexPath, OutlineNode, UICollectionViewListCell) -> Void)
         -> Disposable
         where OutlineNode.NodeType == OutlineNode, Source.Element == [OutlineNode] {
         return { cellProvider in
             _ = self.dataSource
-            let dataSource = OutlineCollectionViewDiffableDataSource(collectionView: base, cellProvider: cellProvider)
+            let dataSource = OutlineCollectionViewDiffableDataSource(collectionView: base, configureCell: cellProvider)
             return self.nodes(dataSource: dataSource)(source)
         }
     }
+
+    public func filteredRootNode<OutlineNode: OutlineNodeType & Hashable, Source: ObservableType>(source: Source, outlineNodeType: OutlineNode.Type = OutlineNode.self) -> Disposable where OutlineNode.NodeType == OutlineNode, Source.Element == OutlineNode {
+        filteredNodes(source: source.map { [$0] })
+    }
     
-    
+    public func filteredNodes<OutlineNode: OutlineNodeType & Hashable, Source: ObservableType>(source: Source) -> Disposable where OutlineNode.NodeType == OutlineNode, Source.Element == [OutlineNode] {
+        source.subscribe(onNext: { nodes in
+            if let outlineDataSource = dataSource.forwardToDelegate() as? OutlineCollectionViewDiffableDataSource<OutlineNode> {
+                outlineDataSource.collectionView(base, observedFilteredEvent: .next(nodes))
+            } else {
+                logger.error("DataSource forwardDelegate must be OutlineCollectionViewDiffableDataSource")
+            }
+        })
+    }
+
     public func nodes<
-            DataSource: RxCollectionViewDataSourceType & UICollectionViewDataSource,
-            Source: ObservableType>
-        (dataSource: DataSource)
+        DataSource: RxCollectionViewDataSourceType & UICollectionViewDataSource,
+        Source: ObservableType
+    >
+    (dataSource: DataSource)
         -> (_ source: Source)
-        -> Disposable where DataSource.Element == Source.Element
-          {
+        -> Disposable where DataSource.Element == Source.Element {
         return { source in
             // This is called for side effects only, and to make sure delegate proxy is in place when
             // data source is being bound.
@@ -95,7 +152,7 @@ extension Reactive where Base: UICollectionView {
             // Therefore it's better to set delegate proxy first, just to be sure.
             _ = self.delegate
             // Strong reference is needed because data source is in use until result subscription is disposed
-            return source.subscribeProxyDataSource(ofObject: self.base, dataSource: dataSource, retainDataSource: true) { [weak collectionView = self.base] (_: RxCollectionViewDataSourceProxy, event) -> Void in
+            return source.subscribeProxyDataSource(ofObject: self.base, dataSource: dataSource, retainDataSource: true) { [weak collectionView = self.base] (_: RxCollectionViewDataSourceProxy, event) in
                 guard let collectionView = collectionView else {
                     return
                 }
@@ -119,8 +176,8 @@ extension ObservableType {
             object.layoutIfNeeded()
         }
 
-        let subscription = self.asObservable()
-            .observe(on:MainScheduler())
+        let subscription = asObservable()
+            .observe(on: MainScheduler())
             .catch { error in
                 bindingError(error)
                 return Observable.empty()
@@ -133,11 +190,11 @@ extension ObservableType {
                 if let object = object {
                     assert(proxy === DelegateProxy.currentDelegate(for: object), "Proxy changed from the time it was first set.\nOriginal: \(proxy)\nExisting: \(String(describing: DelegateProxy.currentDelegate(for: object)))")
                 }
-                
+
                 binding(proxy, event)
-                
+
                 switch event {
-                case .error(let error):
+                case let .error(error):
                     bindingError(error)
                     unregisterDelegate.dispose()
                 case .completed:
@@ -146,7 +203,7 @@ extension ObservableType {
                     break
                 }
             }
-            
+
         return Disposables.create { [weak object] in
             subscription.dispose()
 
@@ -161,17 +218,14 @@ extension ObservableType {
 
 func bindingError(_ error: Swift.Error) {
     let error = "Binding error: \(error)"
-#if DEBUG
+    #if DEBUG
     fatalError(error)
-#else
+    #else
     print(error)
-#endif
+    #endif
 }
 
-
 extension DelegateProxyType {
-
-
     /// Sets forward delegate for `DelegateProxyType` associated with a specific object and return disposable that can be used to unset the forward to delegate.
     /// Using this method will also make sure that potential original object cached selectors are cleared and will report any accidental forward delegate mutations.
     ///
@@ -180,7 +234,7 @@ extension DelegateProxyType {
     /// - parameter onProxyForObject: Object that has `delegate` property.
     /// - returns: Disposable object that can be used to clear forward delegate.
     static func installForwardDelegate(_ forwardDelegate: Delegate, retainDelegate: Bool, onProxyForObject object: ParentObject) -> Disposable {
-        weak var weakForwardDelegate: AnyObject? = forwardDelegate as AnyObject
+//        weak var weakForwardDelegate: AnyObject? = forwardDelegate as AnyObject
         let proxy = self.proxy(for: object)
 
 //        assert(proxy.forwardToDelegate() === nil, "This is a feature to warn you that there is already a delegate (or data source) set somewhere previously. The action you are trying to perform will clear that delegate (data source) and that means that some of your features that depend on that delegate (data source) being set will likely stop working.\n" +
@@ -194,7 +248,7 @@ extension DelegateProxyType {
         return Disposables.create {
             MainScheduler.ensureRunningOnMainThread()
 
-            let delegate: AnyObject? = weakForwardDelegate
+//            let delegate: AnyObject? = weakForwardDelegate
 
 //            assert(delegate == nil || proxy.forwardToDelegate() === delegate, "Delegate was changed from time it was first set. Current \(String(describing: proxy.forwardToDelegate())), and it should have been \(proxy)")
 
@@ -202,6 +256,5 @@ extension DelegateProxyType {
         }
     }
 }
-
 
 #endif
